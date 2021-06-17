0d0343c5c219ca6d7fca5c42c25cf3ac3d1db687
Robin Khiv <robinkhiv@gmail.com>
2021-06-17 08:39:12 -0700

    rk: added header and json content

--- src/backend/backend.php
+++ src/backend/backend.php
@@ -1,7 +1,7 @@
 <?php 
 
-  if(file_exists('../env.php'))
-    include '../env.php';
+  if(file_exists('../../env.php'))
+    include '../../env.php';
 
   $username = getenv('USERNAME');
   $password = getenv('PASSWORD');
@@ -15,9 +15,10 @@
   );
 
   $context = stream_context_create($options);
-
   $content = file_get_contents($url, false, $context);
-
-  print($content);
+  
+  header("Access-Control-Allow-Origin: *");
+  header('Access-Control-Allow-Headers: Content-Type');
+  echo json_encode($content);
   
 ?>
