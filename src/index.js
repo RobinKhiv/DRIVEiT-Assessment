8217dd291a44a312db7f7236e9b5a856853e00f1
Robin Khiv <robinkhiv@gmail.com>
2021-06-17 09:34:01 -0700

    rk: added user context provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { UserProvider } from './context/UserContext';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
