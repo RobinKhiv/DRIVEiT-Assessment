124304b5564b450b71af9d972bb6fcf03d260f9e
Robin Khiv <robinkhiv@gmail.com>
2021-06-18 06:58:27 -0700

    rk: added state for loading

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,7 +3,9 @@ import ApiService from '../services/driveit-api-services';
 
 const UserContext = createContext({
     users: [],
-    updateUserList: () => {}
+    isLoading: false,
+    updateUserList: () => {},
+    setLoading: () => {}
 });
 export default UserContext;
 
@@ -11,16 +13,23 @@ export class UserProvider extends Component {
     constructor(props) {
         super(props);
         const state = {
-            users: []
+            users: [],
+            isLoading: false
         }
         this.state = state;
     }
 
     componentDidMount() {
+        this.setLoading(true);
         ApiService.getData()
             .then(async users => {
                 await this.updateUserList(users.data);
+                this.setLoading(false);
             })
+    }
+
+    setLoading = bool => {
+        this.setState({isLoading: bool});
     }
 
     updateUserList = users => {
@@ -30,7 +39,9 @@ export class UserProvider extends Component {
     render() {
         const value = {
             users: this.state.users,
-            updateUserList: this.updateUserList
+            isLoading: this.state.isLoading,
+            updateUserList: this.updateUserList,
+            setLoading: this.setLoading
         };
 
         return (
