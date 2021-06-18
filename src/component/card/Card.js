47d99cfec9fe9f7ce5c096db14c4d0aff427ce13
Robin Khiv <robinkhiv@gmail.com>
2021-06-17 21:06:13 -0700

    rk: added card component with BEM methology

---
+++ src/component/card/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card(props) {
+  const { user } = props;
+
+  return (
+    <div className="card__item">
+      <img className="card__img" src={user.avatar} alt={`picture of ${user.first_name}`} />
+      <p className="card__text">{user.first_name} {user.last_name}</p>
+      <a href={`mailto:${user.email}`} className="card__email">{user.email}</a>
+    </div>
+  );
+}
