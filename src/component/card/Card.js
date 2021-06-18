3c9e7b068f758062e5c4a503ed39ebc16f89cd53
Robin Khiv <robinkhiv@gmail.com>
2021-06-18 02:39:36 -0700

    rk: added containers for responsive design

--- src/component/card/Card.js
+++ src/component/card/Card.js
@@ -4,10 +4,12 @@ export default function Card(props) {
   const { user } = props;
 
   return (
-    <div className="card__item">
-      <img className="card__img" src={user.avatar} alt={`picture of ${user.first_name}`} />
-      <p className="card__text">{user.first_name} {user.last_name}</p>
-      <a href={`mailto:${user.email}`} className="card__email">{user.email}</a>
+    <div className="card">
+      <div className="card__item">
+          <img className="card__img" src={user.avatar} alt={`picture of ${user.first_name}`} />
+        <p className="card__text">{user.first_name} {user.last_name}</p>
+        <a href={`mailto:${user.email}`} className="card__email">{user.email}</a>
+      </div>
     </div>
   );
 }
