58f426b4f8e4be67725180db6e90c4f0be230015
Robin Khiv <robinkhiv@gmail.com>
2021-06-18 03:55:45 -0700

    rk: updated card component img link

--- src/component/card/Card.js
+++ src/component/card/Card.js
@@ -6,7 +6,7 @@ export default function Card(props) {
   return (
     <div className="card">
       <div className="card__item">
-          <img className="card__img" src={user.avatar} alt={`picture of ${user.first_name}`} />
+          <img className="card__img" src={user.avatar} alt={user.first_name} />
         <p className="card__text">{user.first_name} {user.last_name}</p>
         <a href={`mailto:${user.email}`} className="card__email">{user.email}</a>
       </div>
